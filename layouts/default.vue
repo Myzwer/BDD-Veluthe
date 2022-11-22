4f1ee71e9df8433d2bf90be06ef7d525ad6e7b23
Myzwer <josh@onefortyfivedesign.com>
2022-11-22 02:00:11 -0500

    :construction: Got the click to cloe working on navbar. Dropdown on mobile submenues still doesn't work.

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
         </div>
         <nav>
           <div class="nav-mobile" @click=" open = !open">
-          <!-- Toggle Hamburger between default and X on click (mobile only) -->
+            <!-- Toggle Hamburger between default and X on click (mobile only) -->
             <a
               id="nav-toggle" href="#!"
               :class="{ active: toggleHamburger }"
@@ -17,73 +17,73 @@
           </div>
 
           <VueSlideToggle id="overflow-fix" :open="open" tag="section" :duration="300">
-          <ul class="nav-list" :class="{ block: open }">
+            <ul class="nav-list" :class="{ block: open }">
 
-            <li>
-              <a href="#!">Home</a>
-            </li>
+              <li>
+                <a href="#!">Home</a>
+              </li>
 
-            <li>
-              <a ref="nav-click" href="#!" @click="toggleActiveDropdown('party')">Bridal Party</a>
-              <ul
-                v-show="activeDropdown === 'party'"
-                v-closable="{
-                exclude: ['nav-click'],
-                handler: 'dropdown'
-                }"
-                  class="nav-dropdown"
-              >
-                <li>
-                  <a href="#!">Meet the Couple</a>
-                </li>
-                <li>
-                  <a href="#!">Meet The Bride</a>
-                </li>
-                <li>
-                  <a href="#!">Meet the Groom</a>
-                </li>
-                <li>
-                  <a href="#!">Meet the Bridesmaids</a>
-                </li>
-                <li>
-                  <a href="#!">Meet the Groomsmen</a>
-                </li>
-              </ul>
-            </li>
-
-            <li>
-              <a ref="nav-click-2" href="#!" @click="toggleActiveDropdown('event')">Event</a>
-              <ul
-                v-show="activeDropdown === 'event'"
+              <li>
+                <a ref="navClose" href="#!" @click="toggleActiveDropdown('party')">Bridal Party</a>
+                <ul
+                  v-show="activeDropdown === 'party'"
                   v-closable="{
-                exclude: ['nav-click-2'],
-                handler: 'dropdown'
-                }"
+                  exclude: ['navClose', 'navClose2'],
+                  handler: 'toggleActiveDropdown'
+                  }"
+                  class="nav-dropdown"
+                >
+                  <li>
+                    <a href="#!">Meet the Couple</a>
+                  </li>
+                  <li>
+                    <a href="#!">Meet The Bride</a>
+                  </li>
+                  <li>
+                    <a href="#!">Meet the Groom</a>
+                  </li>
+                  <li>
+                    <a href="#!">Meet the Bridesmaids</a>
+                  </li>
+                  <li>
+                    <a href="#!">Meet the Groomsmen</a>
+                  </li>
+                </ul>
+              </li>
+
+              <li>
+                <a ref="navClose2" href="#!" @click="toggleActiveDropdown('event')">Event</a>
+                <ul
+                  v-show="activeDropdown === 'event'"
+                  v-closable="{
+                  exclude: ['navClose', 'navClose2'],
+                  handler: 'toggleActiveDropdown'
+                  }"
                   class="nav-dropdown">
-                <li>
-                  <a href="#!">Venue Details</a>
-                </li>
-                <li>
-                  <a href="#!">Lodging</a>
-                </li>
-                <li>
-                  <a href="#!">Schedule</a>
-                </li>
-              </ul>
-            </li>
+                  <li>
+                    <a href="#!">Venue Details</a>
+                  </li>
+                  <li>
+                    <a href="#!">Lodging</a>
+                  </li>
+                  <li>
+                    <a href="#!">Schedule</a>
+                  </li>
+                </ul>
+              </li>
 
-            <li>
-              <a href="#!">Contact</a>
-            </li>
+              <li>
+                <a href="#!">Contact</a>
+              </li>
 
-            <li>
-              <a href="#!">Registry</a>
-            </li>
+              <li>
+                <a href="#!">Registry</a>
+              </li>
 
-            <li>
-              <a href="#!">RSVP</a>
-            </li>
-          </ul>
+              <li>
+                <a href="#!">RSVP</a>
+              </li>
+            </ul>
           </VueSlideToggle>
         </nav>
       </div>
@@ -93,7 +93,7 @@
 </template>
 
 <script>
-import { VueSlideToggle } from 'vue-slide-toggle'
+import {VueSlideToggle} from 'vue-slide-toggle'
 
 export default {
   components: {

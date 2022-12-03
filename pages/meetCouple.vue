064ccadea1d04990612d4628f670e5abb984ee1b
Myzwer <josh@onefortyfivedesign.com>
2022-12-02 23:43:52 -0500

    :white_check_mark: Added a new page with test content

---
+++ pages/meetCouple.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<div class="relative">
+			<div class="couple-h1-background">
+				<div class="content-middle text-black text-center">
+					<h2 class="text-2xl font-bold uppercase body-font">Big Day Design</h2>
+					<h1 class="text-4xl mb-7 px-2">Plan your wedding, not your website.</h1>
+					<a
+						href="#"
+						class="bg-purple uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 transition duration-300 ease-in-out hover:bg-purple-hover mt-10"
+					>
+						RSVP
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "MeetCouple"
+}
+</script>
+
+<style>
+.couple-h1-background {
+	background-image: url("~static/images/index/couple-head-bg.jpg");
+	height: 60vh;
+	background-position: center center;
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+</style>

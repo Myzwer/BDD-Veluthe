aa5b693558ed56c18639a84cd16caf13165d4188
Myzwer <josh@onefortyfivedesign.com>
2022-12-06 23:09:58 -0500

    :white_check_mark: Added a component to act as a "learn more" on the bottom of all the pages. Still needs some logic work done.

---
+++ components/ButtonFooter.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+	<div class="bg-secondary-alt">
+		<div class="text-center mx-auto max-w-2xl md:max-w-6xl pt-10 pb-20">
+			<div class="grid grid-cols-12 gap-4">
+
+				<div class="col-span-12 text-center mx-auto">
+					<h3 class="pb-5 text-5xl text-left text-white">View More {{ currentPath }}</h3>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+	name: "ButtonFooter",
+
+	computed: {
+		currentPath() {
+			return this.$nuxt.$route.name
+		},
+
+	}
+}
+</script>

6546137f792c4d14ae6238359365c7057a492d77
Myzwer <josh@onefortyfivedesign.com>
2022-12-17 03:32:11 -0500

    :white_check_mark: Added a component to handle the hotels section and added the hotels json file to handle the data for the component.

---
+++ components/HotelLocations.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="col-span-12 md:col-span-4 text-left">
+        <h3 class="body-font text-xl font-bold">
+            {{ name }}
+        </h3>
+        <h4 class="text-lg">{{ addressLine1 }}</h4>
+        <h4 class="text-lg pb-3">{{ addressLine2 }}</h4>
+
+        <h4 class="text-lg">{{ distance }} away from venue</h4>
+
+        <div class="py-5">
+            <a
+                :href="link"
+                target="_blank"
+                class="bg-accent text-white uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 mt-10 transition duration-300 ease-in-out hover:bg-accent-alt hover:shadow-xl"
+            >
+                View Website
+                <font-awesome-icon
+                    icon="fa-solid fa-arrow-up-right-from-square"
+                />
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HotelLocations',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        addressLine1: {
+            type: String,
+            required: true,
+        },
+        addressLine2: {
+            type: String,
+            required: true,
+        },
+        distance: {
+            type: String,
+            required: true,
+        },
+        link: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>

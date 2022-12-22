b7bf4d7fed7404f8e42be7884680203484851523
Myzwer <josh@onefortyfivedesign.com>
2022-12-22 00:59:33 -0500

    :white_check_mark: Added the registry page and the logic to make it work via a json file.

---
+++ components/RegistryLinks.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="col-span-12 md:col-span-4 text-left text-black">
+        <h3 class="body-font text-2xl font-bold pb-3">
+            {{ store }}
+        </h3>
+        <h4 class="text-lg">{{ item1 }}</h4>
+        <h4 class="text-lg">{{ item2 }}</h4>
+        <h4 class="text-lg">{{ item3 }}</h4>
+
+        <div class="py-5">
+            <a
+                :href="link"
+                target="_blank"
+                class="bg-accent text-white uppercase rounded-md font-bold shadow-lg text-white px-8 py-3 mt-10 transition duration-300 ease-in-out hover:bg-accent-alt hover:shadow-xl"
+            >
+                View Website
+                <font-awesome-icon
+                    icon="fa-solid fa-arrow-up-right-from-square"
+                />
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RegistryLinks',
+    props: {
+        store: {
+            type: String,
+            required: true,
+        },
+        item1: {
+            type: String,
+            required: true,
+        },
+        item2: {
+            type: String,
+            default: null,
+            required: false,
+        },
+        item3: {
+            type: String,
+            default: null,
+            required: false,
+        },
+        link: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
---
+++ pages/WeddingRegistry.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="relative">
+            <div class="registry-h1-background">
+                <div class="content-middle text-white text-center">
+                    <h1 class="text-7xl mb-2 px-2">Registry</h1>
+                    <h2 class="text-xl font-bold uppercase body-font">
+                        #forresterfairytale
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-tertiary">
+            <div class="text-center mx-auto max-w-2xl md:max-w-5xl">
+                <div class="grid grid-cols-12 gap-4 pt-10 pb-12 text-white">
+                    <div class="col-span-12 text-left">
+                        <h2 class="text-5xl text-black">Our Registries</h2>
+                    </div>
+
+                    <RegistryLinks
+                        v-for="reg in registries"
+                        :key="reg.store"
+                        :store="reg.store"
+                        :item1="reg.item1"
+                        :item2="reg.item2"
+                        :item3="reg.item3"
+                        :link="reg.link"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import RegistryLinks from '~/components/RegistryLinks'
+import { registries } from '~/data/registries.json'
+
+export default {
+    name: 'WeddingRegistry',
+    components: { RegistryLinks },
+    data() {
+        return {
+            registries,
+        }
+    },
+}
+</script>
+
+<style>
+/* stylelint-disable color-function-notation */
+.registry-h1-background {
+    background: linear-gradient(rgba(0, 0, 0, 60%), rgba(0, 0, 0, 60%)),
+        url('~static/images/registry/registry-head.jpg');
+    height: 60vh;
+    background-position: top center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+/* stylelint-enable color-function-notation */
+</style>

8158c545532df6cec3a7f09dd4dccd1a6abcf4bb
Myzwer <josh@onefortyfivedesign.com>
2023-01-20 12:47:45 -0500

    :bug: Still yet More troubleshooting of the API key

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
-// eslint-disable-next-line no-console
-console.log('======= GOOGLE_MAPS ========', process.env.NUXT_ENV_MAPS_KEY)
 Vue.use(VueGoogleMaps, {
     load: {
         key: process.env.NUXT_ENV_MAPS_KEY,
